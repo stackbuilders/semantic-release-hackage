d068382e8e8cb3fbf3965c51f3e4338ac94425e9
dalejo96 <dalejo_1996@hotmail.com>
2023-06-02 16:07:37 -0500

    mocha initialization

---
+++ test/hooks.ts
@@ -0,0 +1,13 @@
+// Lifecycle configurations
+
+/* asdf asdfasdfasdf asdf asdfasdfasdf */
+
+/* import Sinon from "sinon";
+
+export const mochaHooks: () => Mocha.RootHookObject = () => {
+  return {
+    afterEach() {
+      Sinon.restore();
+    },
+  };
+}; */
