8ef4f5e0422cd6a65ed19d8932b3ae74a6f41db1
dalejo96 <dalejo_1996@hotmail.com>
2023-08-01 13:37:12 -0500

    verify token

--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -1,9 +1,20 @@
 import { Context } from "semantic-release";
 
+
 import type PluginConfig from "./types/pluginConfig";
 
+const configMessage = "Check the README.md for config info.";
+
+const thrownEnvVarError = (variable: string): Error => {
+  throw new Error(`Environment variable not found: ${variable}. ${configMessage}`);
+};
+
 const verifyConditions = async (pluginConfig: PluginConfig, context: Context): void => {
-  // verifyConditions logic
+  const { HACKAGE_TOKEN } = process.env;
+
+  if (!HACKAGE_TOKEN) {
+    thrownEnvVarError("HACKAGE_TOKEN");
+  }
 };
 
 export default verifyConditions;
