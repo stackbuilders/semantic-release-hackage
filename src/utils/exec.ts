4491eb063423815fe8c10125277a7359668d1cfe
dalejo96 <dalejo_1996@hotmail.com>
2023-10-08 11:38:51 -0500

    requested changes

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,22 +1,9 @@
-import { exec, ExecException } from "child_process";
+import { exec } from "child_process";
 
-const promisifyExec = (command: string): Promise<{ error: string; output: string; }> => {
+export const runExecCommand = (command: string): Promise<{ error: string; output: string; }> => {
   return new Promise((resolve, reject) => {
-    exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve({ error: stderr, output: stdout });
+    exec(command, (error, stdout, stderr) => {
+      error !== null ? reject(error) : resolve({ error: stderr, output: stdout });
     });
   });
-};
-
-export const runExecCommand = async (command: string): Promise<{ error: string; output: string; }> => {
-  try {
-    return await promisifyExec(command);
-  } catch (e) {
-    const error = e as ExecException;
-    throw new Error(error.message);
-  }
 };
