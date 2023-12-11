9988407645263ed1c398294fb7493e0ac2df00c7
dalejo96 <dalejo_1996@hotmail.com>
2023-12-11 08:59:09 -0500

    verifyConditions tests

---
+++ test/integration/verifyConditions.test.ts
@@ -0,0 +1,35 @@
+import { expect } from "@stackbuilders/assertive-ts";
+
+import { PluginConfig } from "../../src/types/pluginConfig";
+import { EnvVarError } from "../../src/utils/EnvVarError";
+import { verifyConditions } from "../../src/verifyConditions"; // Replace with the actual module path
+import { context } from "../utils";
+
+const pluginConfig: PluginConfig = {
+  cabalFile: "test-1-package.cabal",
+  packageName: "test-1-package",
+};
+
+describe("verifyConditions", () => {
+  let originalEnv: NodeJS.ProcessEnv;
+
+  beforeEach(() => {
+    originalEnv = { ...process.env };
+  });
+
+  afterEach(() => {
+    process.env = originalEnv;
+  });
+
+  it("throws EnvVarError when HACKAGE_TOKEN is not defined", () => {
+    delete process.env.HACKAGE_TOKEN;
+
+    expect(() => verifyConditions(pluginConfig, context)).toThrow(new EnvVarError("HACKAGE_TOKEN"));
+  });
+
+  it("does not throw EnvVarError when HACKAGE_TOKEN is defined", () => {
+    process.env.HACKAGE_TOKEN = "test_token";
+
+    expect(() => verifyConditions(pluginConfig, context)).not.toThrow();
+  });
+});
---
+++ test/utils.ts
@@ -0,0 +1,73 @@
+import { Context } from "semantic-release";
+
+export const context: Context = {
+  branch: {
+    channel: "",
+    name: "main",
+    prerelease: false,
+    range: "",
+  },
+  commits: [],
+  env: {},
+  lastRelease: undefined,
+  logger: {
+    await(): void {
+      throw new Error("Function not implemented.");
+    },
+    complete(): void {
+      throw new Error("Function not implemented.");
+    },
+    debug(): void {
+      throw new Error("Function not implemented.");
+    },
+    error(): void {
+      throw new Error("Function not implemented.");
+    },
+    fatal(): void {
+      throw new Error("Function not implemented.");
+    },
+    fav(): void {
+      throw new Error("Function not implemented.");
+    },
+    info(): void {
+      throw new Error("Function not implemented.");
+    },
+    log: () => undefined,
+    note(): void {
+      throw new Error("Function not implemented.");
+    },
+    pause(): void {
+      throw new Error("Function not implemented.");
+    },
+    pending(): void {
+      throw new Error("Function not implemented.");
+    },
+    star(): void {
+      throw new Error("Function not implemented.");
+    },
+    start(): void {
+      throw new Error("Function not implemented.");
+    },
+    success(): void {
+      throw new Error("Function not implemented.");
+    },
+    wait(): void {
+      throw new Error("Function not implemented.");
+    },
+    warn(): void {
+      throw new Error("Function not implemented.");
+    },
+    watch(): void {
+      throw new Error("Function not implemented.");
+    },
+  },
+  nextRelease: {
+    channel: "",
+    gitHead: "h1",
+    gitTag: "v1.0.0",
+    name: "v1.0.0",
+    notes: "",
+    type: "minor",
+    version: "1.0.0",
+  },
+};
