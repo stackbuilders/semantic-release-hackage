7a6c10cdd488487287d11a0e75b4975f88462c57
dalejo96 <dalejo_1996@hotmail.com>
2024-03-11 18:01:04 -0500

    fix(prepare): lookup strategy for Cabal filename

--- src/utils/prepare.ts
+++ src/utils/prepare.ts
@@ -1,9 +1,18 @@
 import fs from "fs";
+import { PrepareContext } from "semantic-release";
 
-export const getCabalFilename: () => string | undefined = () => {
-  return fs
-    .readdirSync(__dirname)
-    .filter(path => fs.statSync(path).isFile())
-    .filter(path => path.endsWith(".cabal"))
+export function lookupCabalFilename(cwd: string, logger: PrepareContext["logger"]): string {
+  const cabalFilename = fs
+    .readdirSync(cwd)
+    .filter((path) => fs.statSync(path).isFile())
+    .filter((path) => path.endsWith(".cabal"))
     .at(0);
-};
+
+  if (!cabalFilename) {
+    logger.error("Unable to find cabal file name in ", cwd);
+    throw new Error("Could not determine the cabal filename. Check the plugin configuration");
+  }
+  logger.info("Using cabal file: ", cabalFilename);
+
+  return cabalFilename;
+}
