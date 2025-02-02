9367579f501de58003c5a202283f1a670181f178
patrick brisbin <pbrisbin@gmail.com>
2025-02-02 10:59:01 -0500

    Add PackageYaml abstraction

---
+++ src/utils/package.ts
@@ -0,0 +1,52 @@
+import YAML from "yaml";
+
+import fs from "fs";
+
+interface HasName {
+  name: string;
+}
+
+interface HasVersion {
+  version: string;
+}
+
+type PackageYamlData = object & HasName & HasVersion;
+
+export class PackageYaml {
+  private readonly path: string;
+  private readonly data: PackageYamlData;
+
+  public constructor(path?: string) {
+    this.path = path ?? "package.yaml";
+    this.data = this.read();
+  }
+
+  public getName(): string {
+    return this.data.name;
+  }
+
+  public getVersion(): string {
+    return this.data.version;
+  }
+
+  public setVersion(version: string): PackageYaml {
+    this.data.version = version;
+    return this;
+  }
+
+  public write(path?: string): PackageYaml {
+    fs.writeFileSync(path ?? this.path, YAML.stringify(this.data));
+    return this;
+  }
+
+  private read(): PackageYamlData {
+    const exists = fs.existsSync(this.path);
+
+    if (!exists) {
+      throw new Error(`package.yaml does not exist in ${process.cwd()}`);
+    }
+
+    const yaml = fs.readFileSync(this.path, "utf8");
+    return YAML.parse(yaml) as PackageYamlData;
+  }
+}
---
+++ test/unit/package.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "@assertive-ts/core";
+
+import { PackageYaml } from "../../src/utils/package";
+
+describe("PackageYaml", () => {
+  it("reads an example file", () => {
+    const packageYaml = new PackageYaml("test/fixtures/test-package.yaml");
+
+    expect(packageYaml.getName()).toBeEqual("shellwords");
+  });
+
+  it("can be updated and rewritten", () => {
+    const packageYamlPath = "test/fixtures/test-package.yaml";
+    const packageYaml = new PackageYaml(packageYamlPath);
+    const originalVersion = packageYaml.getVersion();
+    const updatedVersion = "0.1.42.42";
+
+    packageYaml.setVersion(updatedVersion);
+    packageYaml.write();
+
+    const packageYaml2 = new PackageYaml(packageYamlPath);
+
+    expect(packageYaml2.getVersion()).toBeEqual(updatedVersion);
+
+    // cleanup
+    packageYaml2.setVersion(originalVersion);
+    packageYaml2.write();
+  });
+});
